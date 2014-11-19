c604293eeb0a2488b6d5b0afabb69accf82aaba6
worldbreak <worldbreak@seznam.cz>
2014-11-19 22:53:16 +0100

    unbelievable

--- src/Generation.java
+++ src/Generation.java
@@ -63,25 +63,44 @@ public class Generation {
     }
 
     public Generation Crossing(double probability){
-        Generation g = this;
         Random rnd = new Random();
         boolean end = false;
         int j;
+        System.out.println("Krizeni pred:");
+        for (int n=0;n<lengthGen;n++){
+
+            //  System.out.print(g2.parents[j].fitnesFunction()+" ");
+            System.out.println(Arrays.toString(this.parents[n].ch));
+        }
         do{
-           int i=rnd.nextInt(lengthGen);
+           int i= rnd.nextInt(lengthGen);
            do {
              j=rnd.nextInt(lengthGen);
            } while (i==j);
-
+            System.out.println("i: "+i+" j:"+j);
             double rand = rnd.nextDouble();
             //       System.out.println("Prohazuji "+i+" s "+j+". Od pozice "+randomNum);
             if (probability>rand){
                 int randomNum = rnd.nextInt(this.lengthChrom-2)+1;
+            //    System.out.println("randomnum: "+randomNum);
+                int[] ch1 = this.parents[i].ch;
+                int[] ch2 = this.parents[j].ch;
                 for (int k=randomNum;k<lengthChrom;k++){
-                    int a = this.parents[j].ch[k];
-                    g.parents[j].ch[k] = this.parents[i].ch[k];
-                    g.parents[i].ch[k] = a;
+
+                    int a = ch1[k];
+                    ch1[k] = ch2[k];
+                    ch2[k] = a;
+                    System.out.println(Arrays.toString(this.parents[3].ch));
+                  //  System.out.println("k="+k);
+
                 }
+                this.parents[i].ch=ch1;
+                this.parents[j].ch=ch2;
+                System.out.println("Krizeni po:");
+                for (int n=0;n<lengthGen;n++) {
+                    System.out.println(Arrays.toString(this.parents[n].ch));
+                }
+                System.out.println();
                 end = true;
             }
         }while(end==false);
@@ -105,7 +124,7 @@ public class Generation {
                 }
             }
         }*/
-        return g;
+        return this;
     }
 
     public Generation Mutation(double probalitity){
@@ -122,6 +141,10 @@ public class Generation {
             {
                 g.parents[randomCH].ch[randomA]=0;
             }
+        }
+        for (int j=0;j<lengthGen;j++){
+            //  System.out.print(g2.parents[j].fitnesFunction()+" ");
+            System.out.println(Arrays.toString(g.parents[j].ch));
         }
         return g;
     }
--- src/geneticAlgorithm.java
+++ src/geneticAlgorithm.java
@@ -5,31 +5,32 @@ import java.util.Arrays;
  * Created by Lada on 15.11.14.
  */
 public class geneticAlgorithm {
-    int numberOfGenerations = 3;
-    int numberOfChromozoms = 3;
+    int numberOfGenerations = 4;
+    int numberOfChromozoms = 8;
     Generation g = new Generation(numberOfGenerations,numberOfChromozoms, true);
     Generation g2 = new Generation();
     double lastSum=0;
     int pocet2=0;
+
     public geneticAlgorithm(){
             g2=g;
-        for (int i=0;i<3;i++){
+        for (int i=0;i<100;i++){
             System.out.println(i+". generace: ");
             for (int j=0;j<numberOfGenerations;j++){
               //  System.out.print(g2.parents[j].fitnesFunction()+" ");
                 System.out.println(Arrays.toString(g2.parents[j].ch));
             }
             g2 = g2.Reproduction();
-            g2 = g2.Crossing(0.6);
-            g2 = g2.Mutation(0.1);
-            double sum = 0;
-            double[] array = g2.GetFittnesFunctionForAllParents(false);
-            for (int j = 0; j < numberOfGenerations; j++){
-                sum = sum + array[j];
-                if (i==0) lastSum=sum;
+            g2 = g2.Crossing(1);
+            g2 = g2.Mutation(0.2);
+            int sum=0;
+            for (int h=0;h<numberOfGenerations;h++)
+            for (int k=0;k<numberOfChromozoms;k++) {
+                if (g2.parents[h].ch[k] == 1) sum++;
             }
-            System.out.println(sum);
-       /*     if (lastSum == sum)
+            if (sum == numberOfGenerations*numberOfChromozoms) break;
+            double[] array = g2.GetFittnesFunctionForAllParents(false);
+            /*     if (lastSum == sum)
                 pocet2++;
             else pocet2=0;
             lastSum = sum;
